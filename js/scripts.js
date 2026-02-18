cdb5bbca5d5b278f663fdf02866ead59385fb9f5
Eoin <mcleanee@hendrix.edu>
2026-02-18 12:40:00 -0600

    js fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,4 @@
-/*!
-* Start Bootstrap - Landing Page v6.0.6 (https://startbootstrap.com/theme/landing-page)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
 $(document).ready(function () {
-
-
     function keywordFromWeatherCode(code) {
         if (code === 0) return "sunlight";
         if (code === 1 || code === 2) return "Clear";
